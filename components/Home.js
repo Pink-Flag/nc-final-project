c1efd4b4ca3138b2a47b899caed1bfb24bad455e
mgrindrod92 <matt@grindrod.me.uk>
2022-06-29 14:37:16 +0100

    add buttons/links for homepage

--- components/Home.js
+++ components/Home.js
@@ -1,14 +1,82 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
+import { Link } from "react-router-native";
 
 const Home = () => {
+
+
   return (
+    <>
     <View>
-      <Text>Home</Text>
+      <Text style={styles.welcomeMessage}>Welcome, username!</Text>
     </View>
+
+<View style={styles.buttonContainer}>
+          <TouchableOpacity
+            style={[styles.button, styles.buttonOutline]}
+          >
+            <Link to="/individualdeck">
+            <Text style={styles.buttonOutlineText}>Create new deck</Text>
+            </Link>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[styles.button, styles.buttonOutline]}
+          >
+         <Link to="/viewdecks">
+         <Text style={styles.buttonOutlineText}>View existing decks</Text>
+            </Link>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={[styles.button, styles.buttonOutline]}
+          >
+            <Link to="/profile">
+            <Text style={styles.buttonOutlineText}>View profile</Text>
+            </Link>
+          </TouchableOpacity>
+        </View>
+</>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  
+  buttonContainer: {
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: "#5c6784",
+    borderWidth: 2,
+  },
+  buttonOutlineText: {
+    color: "#5c6784",
+    fontWeight: "700",
+    fontSize: 16,
+  }, 
+  button: {
+    backgroundColor: "#5c6784",
+    width: "100%",
+    padding: 20,
+    borderRadius: 10,
+    alignItems: "center",
+    minWidth: "65%",
+    margin: 25
+  },
+  welcomeMessage: {
+    fontSize: 20,
+    padding: 15,
+  }
+});
