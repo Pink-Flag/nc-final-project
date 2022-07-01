76e22011729dac8745a1b61691cee0df8844d725
Kristina Petrova <kristina.petrova13@gmail.com>
2022-07-01 09:32:59 +0100

    implement structure viewDecks

--- components/PairsTest.js
+++ components/PairsTest.js
@@ -1,14 +1,108 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const PairsTest = () => {
+  const navigate = useNavigate();
+  const words = [
+    { word: "Apfel", definition: "Apple" },
+    { word: "Orange", definition: "Orange" },
+    { word: "Banane", definition: "Banana" },
+    { word: "Tomate", definition: "Tomato" },
+    { word: "Erdbeere", definition: "Strawberry" },
+    { word: "Banane", definition: "Banana" },
+    { word: "Tomate", definition: "Tomato" }
+  ]
+
   return (
-    <View>
-      <Text>PairsTest</Text>
+    <>
+    <View style={styles.wordsContainer}>
+
+      {words.map((word) =>{
+       return <>
+      
+        <TouchableOpacity
+            style={[styles.buttonWord, styles.buttonOutline]}
+            key={word.word}
+          >
+            <Text style={styles.buttonOutlineText}>{word.word}</Text>
+          </TouchableOpacity>
+        <TouchableOpacity
+            style={[styles.buttonWord, styles.buttonOutline]}
+            key={word.definition}
+          >
+            <Text style={styles.buttonOutlineText}>{word.definition}</Text>
+          </TouchableOpacity>
+       
+        </>
+
+      })}
     </View>
+    <View style={styles.buttonContainer}>
+    
+          <TouchableOpacity
+            style={[styles.button, styles.buttonOutline]}
+             onPress={() => {
+              navigate('/testing');
+          }}
+          >
+            <Text style={styles.buttonOutlineText}>Back</Text>
+          </TouchableOpacity>
+      
+        </View>
+    </>
   );
 };
 
 export default PairsTest;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: "60%",
+    justifyContent : "center",
+    alignItems: "center",
+    marginTop: 30,
+    marginBottom:10,
+  
+
+  },
+  wordsContainer: {
+      flexDirection: "row",
+      flexWrap: "wrap",
+      marginLeft: "8%",
+      marginTop: "10%",
+  },
+  button: {
+    backgroundColor: "#5c6784",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    minWidth: "65%",
+  },
+  buttonWord: {
+    backgroundColor: "#5c6784",
+    width: "35%",
+    padding: 12,
+    borderRadius: 10,
+    alignItems: "center",
+    minWidth: "35%",
+    margin: 15,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: "#5c6784",
+    borderWidth: 2,
+  },
+  buttonOutlineText: {
+    color: "#5c6784",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+});
--- components/VanillaTest.js
+++ components/VanillaTest.js
@@ -1,14 +1,84 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const VanillaTest = () => {
+
+  const navigate = useNavigate();
+
   return (
-    <View>
-      <Text>VanillaTest</Text>
+    <>
+    <View style={styles.card}>
+      <Text style={styles.text}>Die Katze</Text>
+      <Text style={styles.text}>plural: die Katzen</Text>
     </View>
+    <View>
+      <Text style={styles.text}>
+        Showing card 3 of 20
+      </Text>
+    </View>
+    <View style={styles.buttonContainer}>
+    
+          <TouchableOpacity
+            style={[styles.button, styles.buttonOutline]}
+             onPress={() => {
+              navigate('/testing');
+          }}
+          >
+            <Text style={styles.buttonOutlineText}>Back</Text>
+          </TouchableOpacity>
+      
+        </View>
+    </>
   );
 };
 
 export default VanillaTest;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  card: {
+    border: 2,
+    backgroundColor: "white",
+    borderWidth: 3,
+    borderRadius: 10,
+    padding: 25,
+    width: "80%",
+    marginTop: 30,
+  },
+  text : {
+    fontSize: 24,
+    textAlign: "center",
+    padding: 20,
+
+  },
+  buttonContainer: {
+    width: "60%",
+    justifyContent : "center",
+    alignItems: "center",
+    marginTop: 170,
+    },
+  button: {
+    backgroundColor: "#5c6784",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    minWidth: "65%",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: "#5c6784",
+    borderWidth: 2,
+  },
+  buttonOutlineText: {
+    color: "#5c6784",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+});
